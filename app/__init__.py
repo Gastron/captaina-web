b04fc3d14b0bf261763042a5fe34e764b0039c01
Aku Rouhe <aku.rouhe@aalto.fi>
2018-06-05 17:43:02 +0300

    remove old

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt
-
-app = Flask(__name__, instance_relative_config=True)
-
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
-
-flask_bcrypt = Bcrypt(app)
