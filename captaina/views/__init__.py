279d724e8a8946dfabab792e3df8061866e789aa
Aku Rouhe <aku.rouhe@aalto.fi>
2018-06-19 18:29:43 +0300

    Show and redirect to prompts

--- captaina/views/__init__.py
+++ captaina/views/__init__.py
@@ -1,8 +1,10 @@
 from .login import login_bp
 from .dashboard import dashboard_bp
 from .feedback import feedback_bp
+from .lesson import lesson_bp
 
 def register_blueprints(app):
     app.register_blueprint(login_bp)
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(feedback_bp)
+    app.register_blueprint(lesson_bp, url_prefix='/lessons')
