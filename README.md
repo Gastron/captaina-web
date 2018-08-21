2d67592a8f195f2348eb9ff0523764477da49a2c
Aku Rouhe <aku.rouhe@aalto.fi>
2018-08-22 00:40:39 +0300

    Update state of repo, remove dev material

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # The website front and backend for Captaina CAPT application
 
-## Known issues:
-- Sometimes test\_login fails, because the CSRF token is invalid. The token extraction seems to work, but for some reason it is not accepted on the server side.
--- debug_instance/config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf8 -*-
-MONGO_DATABASE_URI = "mongodb://root:debug@db:27017/debug?authSource=admin"
-SECRET_KEY = "ikudsahvdsanör283<..1498"
-SPEECH_SERVER_URL = "ws:localhost:5443/client/ws/speech"
-STATUS_SERVER_URL = "ws:localhost:5443/client/ws/status"
