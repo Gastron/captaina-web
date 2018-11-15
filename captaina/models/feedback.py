0ae819c7c95a4aad1a5ac4c713927c05264391d3
Aku Rouhe <aku.rouhe@aalto.fi>
2018-11-15 15:40:50 +0200

    CLI for reading feedback

--- captaina/models/feedback.py
+++ captaina/models/feedback.py
@@ -1,11 +1,13 @@
 import pymodm as modm
 import pymongo as mongo
 from datetime import datetime
+from ..jinja_filters import datetimefmt
 
 class Feedback(modm.MongoModel):
     message = modm.fields.CharField(required = True)
     created = modm.fields.DateTimeField(default = datetime.now)
     modified  = modm.fields.DateTimeField(default = datetime.now)
+    read = modm.fields.BooleanField(default = False)
 
     def save(self, *args, **kwargs):
         self.modified = datetime.now()
@@ -13,3 +15,10 @@ class Feedback(modm.MongoModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def pretty_print(self):
+        print()
+        print("="*80)
+        print("Feedback, last modified on:", datetimefmt(self.modified))
+        print("=== Message: ===")
+        print(self.message)
